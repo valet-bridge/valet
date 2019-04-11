1252a7b54ef47b7c546fad4eb96e7570fa8a19fd
Soren Hein <soren.hein@gmail.com>
2019-04-11 19:38:19 +0200

    Added script to read German Merlin files

---
+++ scripts/Germany/getfiles.pl
@@ -0,0 +1,36 @@
+#!perl
+
+use strict;
+use warnings;
+use v5.10;
+
+# Useful for downloading Merlin files.
+
+use File::Fetch;
+
+# 2019 Bundesliga
+my $prefix = 'http://www.bridge-verband.de/images/uploads/ftp4308/';
+my $dprefix = '19_01_19_team_bdz_';
+my $sprefix = 'liga2019';
+
+my $fetch = 'vugraph_linfetch.php';
+
+for my $k (1 .. 5)
+{
+  for my $d (1 .. 9)
+  {
+    for my $bd (1 .. 16)
+    {
+      my $ext = "k${k}_d${d}_bd_${bd}.html";
+      my $download = $dprefix . $ext;
+      my $name = $prefix . $download;
+      say $name;
+
+      my $ff = File::Fetch->new(uri => $name); 
+      my $where = $ff->fetch(to => $sprefix) or die $ff->error;
+
+      rename "$sprefix/$download", "$sprefix/$ext";
+    }
+  }
+}
+
