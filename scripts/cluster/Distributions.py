60a0f0ed70f88339892b437f537af2f7b2b90bb0
Soren Hein <soren.hein@gmail.com>
2023-08-16 20:47:23 +0200

    Permit Any.txt files.  Updates to distribution naming

--- scripts/cluster/Distributions.py
+++ scripts/cluster/Distributions.py
@@ -1,187 +1,199 @@
 from enum import Enum
 
 class DISTRIBUTIONS(Enum):
-  DIST_4432 = 0
+  DIST_44MAJ32 = 0
+  DIST_4MAJ4MIN32 = 1
+  DIST_44MIN32 = 2
 
-  DIST_5MAJ332 = 1
-  DIST_5MIN332 = 2
+  DIST_5MAJ332 = 3
+  DIST_5MIN332 = 4
 
-  DIST_5MAJ431 = 3
-  DIST_5MIN431 = 4
+  DIST_54MAJ31 = 5
+  DIST_5MAJ4MIN31 = 6
+  DIST_4MAJ5MIN31 = 7
+  DIST_54MIN31 = 8
 
-  DIST_5MAJ422 = 5
-  DIST_5MIN422 = 6
+  DIST_54MAJ22 = 9
+  DIST_5MAJ4MIN22 = 10
+  DIST_4MAJ5MIN22 = 11
+  DIST_54MIN22 = 12
 
-  DIST_4333 = 7
+  DIST_4MAJ333 = 13
+  DIST_4MIN333 = 14
 
-  DIST_6MAJ322 = 8
-  DIST_6MIN322 = 9
+  DIST_6MAJ322 = 15
+  DIST_6MIN322 = 16
 
-  DIST_6MAJ421 = 10
-  DIST_6MIN421 = 11
+  DIST_64MAJ21 = 17
+  DIST_6MAJ4MIN21 = 18
+  DIST_4MAJ6MIN21 = 19
+  DIST_64MIN21 = 20
 
-  DIST_6MAJ331 = 12
-  DIST_6MIN331 = 13
+  DIST_6MAJ331 = 21
+  DIST_6MIN331 = 22
 
-  DIST_55MAJ21 = 14
-  DIST_5MAJ5MIN21 = 15
-  DIST_55MIN21 = 16
+  DIST_55MAJ21 = 23
+  DIST_5MAJ5MIN21 = 24
+  DIST_55MIN21 = 25
 
-  DIST_4441 = 17
+  DIST_44MAJ41 = 26
+  DIST_44MIN41 = 27
 
-  DIST_7MAJ321 = 18
-  DIST_7MIN321 = 19
+  DIST_7MAJ321 = 28
+  DIST_7MIN321 = 29
 
-  DIST_6MAJ430 = 20
-  DIST_6MIN430 = 21
+  DIST_64MAJ30 = 30
+  DIST_6MAJ4MIN30 = 31
+  DIST_4MAJ6MIN30 = 32
+  DIST_64MIN30 = 33
 
-  DIST_54MAJ40 = 22
-  DIST_50MAJ44 = 23
-  DIST_44MAJ50 = 24
-  DIST_40MAJ54 = 25
+  DIST_54MAJ40 = 34
+  DIST_50MAJ44 = 35
+  DIST_44MAJ50 = 36
+  DIST_40MAJ54 = 37
 
-  DIST_55MAJ30 = 26
-  DIST_53MAJ50 = 27
-  DIST_50MAJ53 = 28
-  DIST_55MIN30 = 29
+  DIST_55MAJ30 = 38
+  DIST_53MAJ50 = 39
+  DIST_50MAJ53 = 40
+  DIST_55MIN30 = 41
 
-  DIST_65MAJ11 = 30
-  DIST_61MAJ51 = 31
-  DIST_51MAJ61 = 32
-  DIST_65MIN11 = 33
+  DIST_65MAJ11 = 42
+  DIST_6MAJ5MIN11 = 43
+  DIST_5MAJ6MIN11 = 44
+  DIST_65MIN11 = 45
 
-  DIST_65MAJ20 = 34
-  DIST_6MAJ5MIN20 = 35
-  DIST_5MAJ6MIN20 = 36
-  DIST_65MIN20 = 37
+  DIST_65MAJ20 = 46
+  DIST_6MAJ5MIN20 = 47
+  DIST_5MAJ6MIN20 = 48
+  DIST_65MIN20 = 49
 
-  DIST_7MAJ222 = 38
-  DIST_7MIN222 = 39
+  DIST_7MAJ222 = 50
+  DIST_7MIN222 = 51
 
-  DIST_74MAJ11 = 40
-  DIST_71MAJ41 = 41
-  DIST_74MIN11 = 42
-  DIST_71MIN41 = 43
+  DIST_74MAJ11 = 52
+  DIST_7MAJ4MIN11 = 53
+  DIST_4MAJ7MIN11 = 54
+  DIST_74MIN11 = 55
 
-  DIST_74MAJ20 = 44
-  DIST_7MAJ4MIN20 = 45
-  DIST_4MAJ7MIN20 = 46
-  DIST_74MIN20 = 47
+  DIST_74MAJ20 = 56
+  DIST_7MAJ4MIN20 = 57
+  DIST_4MAJ7MIN20 = 58
+  DIST_74MIN20 = 59
 
-  DIST_7MAJ330 = 48
-  DIST_7MIN330 = 49
+  DIST_7MAJ330 = 60
+  DIST_7MIN330 = 61
 
-  DIST_8MAJ221 = 50
-  DIST_8MIN221 = 51
+  DIST_8MAJ221 = 62
+  DIST_8MIN221 = 63
 
-  DIST_8MAJ311 = 52
-  DIST_8MIN311 = 53
+  DIST_8MAJ311 = 64
+  DIST_8MIN311 = 65
 
-  DIST_75MAJ10 = 54
-  DIST_7MAJ5MIN10 = 55
-  DIST_5MAJ7MIN10 = 56
-  DIST_75MIN10 = 57
+  DIST_75MAJ = 66
+  DIST_7MAJ5MIN = 67
+  DIST_5MAJ7MIN = 68
+  DIST_75MIN = 69
 
-  DIST_8MAJ320 = 58
-  DIST_8MIN320 = 59
+  DIST_8MAJ320 = 70
+  DIST_8MIN320 = 71
 
-  DIST_66MAJ10 = 60
-  DIST_6MAJ6MIN10 = 61
-  DIST_66MIN10 = 62
+  DIST_66MAJ = 72
+  DIST_6MAJ6MIN = 73
+  DIST_66MIN = 74
 
-  DIST_84MAJ10 = 63
-  DIST_8MAJ4MIN10 = 64
-  DIST_4MAJ8MIN10 = 65
-  DIST_84MIN10 = 66
+  DIST_84MAJ = 75
+  DIST_8MAJ4MIN = 76
+  DIST_4MAJ8MIN = 77
+  DIST_84MIN = 78
 
-  DIST_9MAJ211 = 67
-  DIST_9MIN211 = 68
+  DIST_9MAJ211 = 79
+  DIST_9MIN211 = 80
 
-  DIST_9MAJ310 = 69
-  DIST_9MIN310 = 70
+  DIST_9MAJ310 = 81
+  DIST_9MIN310 = 82
 
-  DIST_9MAJ220 = 71
-  DIST_9MIN220 = 72
+  DIST_9MAJ220 = 83
+  DIST_9MIN220 = 84
 
-  DIST_76MAJ00 = 73
-  DIST_7MAJ6MIN = 74
-  DIST_6MAJ7MIN = 75
-  DIST_76MIN00 = 76
+  DIST_76MAJ = 85
+  DIST_7MAJ6MIN = 86
+  DIST_6MAJ7MIN = 87
+  DIST_76MIN = 88
 
-  DIST_85MAJ00 = 77
-  DIST_8MAJ5MIN = 78
-  DIST_5MAJ8MIN = 79
-  DIST_85MIN00 = 80
+  DIST_85MAJ = 89
+  DIST_8MAJ5MIN = 90
+  DIST_5MAJ8MIN = 91
+  DIST_85MIN = 92
 
-  DIST_10MAJ210 = 81
-  DIST_10MIN210 = 82
+  DIST_10MAJ210 = 93
+  DIST_10MIN210 = 94
 
-  DIST_94MAJ00 = 83
-  DIST_9MAJ4MIN = 84
-  DIST_4MAJ9MIN = 85
-  DIST_94MIN00 = 86
+  DIST_94MAJ = 95
+  DIST_9MAJ4MIN = 96
+  DIST_4MAJ9MIN = 97
+  DIST_94MIN = 98
 
-  DIST_10MAJ111 = 87
-  DIST_10MIN111 = 88
+  DIST_10MAJ111 = 99
+  DIST_10MIN111 = 100
 
-  DIST_10MAJ300 = 89
-  DIST_10MIN300 = 90
+  DIST_10MAJ300 = 101
+  DIST_10MIN300 = 102
 
-  DIST_11MAJ110 = 91
-  DIST_11MIN110 = 92
+  DIST_11MAJ110 = 103
+  DIST_11MIN110 = 104
 
-  DIST_11MAJ200 = 93
-  DIST_11MIN200 = 94
+  DIST_11MAJ200 = 105
+  DIST_11MIN200 = 106
 
-  DIST_12MAJ100 = 95
-  DIST_12MIN100 = 96
+  DIST_12MAJ = 107
+  DIST_12MIN = 108
 
-  DIST_13MAJ000 = 97
-  DIST_13MIN000 = 98
+  DIST_13MAJ = 109
+  DIST_13MIN = 110
 
-  DIST_SIZE = 99
+  DIST_SIZE = 111
 
 
 DISTRIBUTION_NAMES = [
-  "4432",
+  "44MAJ32", "4MAJ4MIN32", "44MIN32",
   "5MAJ332", "5MIN332",
-  "5MAJ431", "5MIN431",
-  "5MAJ422", "5MIN422",
-  "4333",
+  "54MAJ31", "5MAJ4MIN31", "4MAJ5MIN31", "54MIN31",
+  "54MAJ22", "5MAJ4MIN22", "4MAJ5MIN22", "54MIN22",
+  "4MAJ333", "4MIN333",
   "6MAJ322", "6MIN322",
-  "6MAJ421", "6MIN421",
+  "64MAJ21", "6MAJ4MIN21", "4MAJ6MIN21", "64MIN21",
   "6MAJ331", "6MIN331",
   "55MAJ21", "5MAJ5MIN21", "55MIN21",
-  "4441",
+  "44MAJ41", "44MIN41",
   "7MAJ321", "7MIN321",
-  "6MAJ430", "6MIN430",
+  "64MAJ30", "6MAJ4MIN30", "4MAJ6MIN30", "64MIN30",
   "54MAJ40", "50MAJ44", "44MAJ50", "40MAJ54",
   "55MAJ30", "53MAJ50", "50MAJ53", "55MIN30",
-  "65MAJ11", "61MAJ51", "51MAJ61", "65MIN11",
+  "65MAJ11", "6MAJ5MIN11", "5MAJ6MIN11", "65MIN11",
   "65MAJ20", "6MAJ5MIN20", "5MAJ6MIN20", "65MIN20",
   "7MAJ222", "7MIN222",
-  "74MAJ11", "71MAJ41", "74MIN11", "71MIN41",
+  "74MAJ11", "7MAJ4MIN11", "4MAJ7MIN11", "74MIN11",
   "74MAJ20", "7MAJ4MIN20", "4MAJ7MIN20", "74MIN20",
   "7MAJ330", "7MIN330",
   "8MAJ221", "8MIN221",
   "8MAJ311", "8MIN311",
-  "75MAJ10", "7MAJ5MIN10", "5MAJ7MIN10", "75MIN10",
+  "75MAJ", "7MAJ5MIN", "5MAJ7MIN", "75MIN",
   "8MAJ320", "8MIN320",
-  "66MAJ10", "6MAJ6MIN10", "66MIN10",
-  "84MAJ10", "8MAJ4MIN10", "4MAJ8MIN10", "84MIN10",
+  "66MAJ", "6MAJ6MIN", "66MIN",
+  "84MAJ", "8MAJ4MIN", "4MAJ8MIN", "84MIN",
   "9MAJ211", "9MIN211",
   "9MAJ310", "9MIN310",
   "9MAJ220", "9MIN220",
-  "76MAJ00", "7MAJ6MIN", "6MAJ7MIN", "76MIN00",
-  "85MAJ00", "8MAJ5MIN00", "5MAJ8MIN00", "85MIN00",
+  "76MAJ", "7MAJ6MIN", "6MAJ7MIN", "76MIN",
+  "85MAJ", "8MAJ5MIN", "5MAJ8MIN", "85MIN",
   "10MAJ210", "10MIN210",
-  "94MAJ00", "9MAJ4MIN00", "4MAJ9MIN00", "94MIN00",
+  "94MAJ", "9MAJ4MIN", "4MAJ9MIN", "94MIN",
   "10MAJ111", "10MIN111",
   "10MAJ300", "10MIN300",
   "11MAJ110", "11MIN110",
   "11MAJ200", "11MIN200",
-  "12MAJ100", "12MIN100",
-  "13MAJ000", "13MIN000",
+  "12MAJ", "12MIN",
+  "13MAJ", "13MIN"
   ]
 
 # Tired of enums...
@@ -204,11 +216,21 @@ class Distribution:
   
   def number4(self, params):
     if (params[DPAR_MIN_LEN] == 1):
-      return DISTRIBUTIONS.DIST_4441.value
+      if (params[DPAR_MIN_MAJOR] == 4):
+        return DISTRIBUTIONS.DIST_44MAJ41.value
+      else:
+        return DISTRIBUTIONS.DIST_44MIN41.value
     elif (params[DPAR_MIN_LEN] == 2):
-      return DISTRIBUTIONS.DIST_4432.value
+      if (params[DPAR_MIN_MAJOR] == 4):
+        return DISTRIBUTIONS.DIST_44MAJ32.value
+      elif (params[DPAR_MIN_MINOR] == 4):
+        return DISTRIBUTIONS.DIST_44MIN32.value
+      else:
+        return DISTRIBUTIONS.DIST_4MAJ4MIN32.value
+    elif (params[DPAR_MAX_MAJOR] == 4):
+      return DISTRIBUTIONS.DIST_4MAJ333.value
     else:
-      return DISTRIBUTIONS.DIST_4333.value
+      return DISTRIBUTIONS.DIST_4MIN333.value
 
   
   def number5major(self, params):
@@ -230,11 +252,15 @@ class Distribution:
         return DISTRIBUTIONS.DIST_55MAJ21.value
       elif (params[DPAR_MAX_MINOR] == 5):
         return DISTRIBUTIONS.DIST_5MAJ5MIN21.value
+      elif (params[DPAR_MIN_MAJOR] == 4):
+        return DISTRIBUTIONS.DIST_54MAJ31.value
       else:
-        return DISTRIBUTIONS.DIST_5MAJ431.value
+        return DISTRIBUTIONS.DIST_5MAJ4MIN31.value
     elif (params[DPAR_MIN_LEN] == 2):
-      if (params[DPAR_MAX_MINOR] == 2 or params[DPAR_MAX_MINOR] == 4):
-        return DISTRIBUTIONS.DIST_5MAJ422.value
+      if (params[DPAR_MAX_MINOR] == 2):
+        return DISTRIBUTIONS.DIST_54MAJ22.value
+      if (params[DPAR_MAX_MINOR] == 4):
+        return DISTRIBUTIONS.DIST_5MAJ4MIN22.value
       else:
         return DISTRIBUTIONS.DIST_5MAJ332.value
     else:
@@ -254,11 +280,15 @@ class Distribution:
     elif (params[DPAR_MIN_LEN] == 1):
       if (params[DPAR_MIN_MINOR] == 5):
         return DISTRIBUTIONS.DIST_55MIN21.value
+      elif (params[DPAR_MAX_MAJOR] == 4):
+        return DISTRIBUTIONS.DIST_4MAJ5MIN31.value
       else:
-        return DISTRIBUTIONS.DIST_5MIN431.value
+        return DISTRIBUTIONS.DIST_54MIN31.value
     elif (params[DPAR_MIN_LEN] == 2):
-      if (params[DPAR_MAX_MAJOR] == 4 or params[DPAR_MIN_MINOR] == 4):
-        return DISTRIBUTIONS.DIST_5MIN422.value
+      if (params[DPAR_MAX_MAJOR] == 4):
+        return DISTRIBUTIONS.DIST_4MAJ5MIN22.value
+      elif (params[DPAR_MIN_MINOR] == 4):
+        return DISTRIBUTIONS.DIST_54MIN22.value
       else:
         return DISTRIBUTIONS.DIST_5MIN332.value
     else:
@@ -270,23 +300,27 @@ class Distribution:
       return DISTRIBUTIONS.DIST_6MAJ322.value
     elif (params[DPAR_MIN_LEN] == 1):
       if (params[DPAR_MAX_MINOR] == 5):
-        return DISTRIBUTIONS.DIST_61MAJ51.value
+        return DISTRIBUTIONS.DIST_6MAJ5MIN11.value
       elif (params[DPAR_MAX_MINOR] == 3):
         return DISTRIBUTIONS.DIST_6MAJ331.value
       elif (params[DPAR_MAX_MINOR] == 1):
         return DISTRIBUTIONS.DIST_65MAJ11.value
+      elif (params[DPAR_MIN_MAJOR] == 4):
+        return DISTRIBUTIONS.DIST_64MAJ21.value
       else:
-        return DISTRIBUTIONS.DIST_6MAJ421.value
+        return DISTRIBUTIONS.DIST_6MAJ4MIN21.value
     elif (params[DPAR_MAX_MINOR] == 6):
-      return DISTRIBUTIONS.DIST_6MAJ6MIN10.value
+      return DISTRIBUTIONS.DIST_6MAJ6MIN.value
     elif (params[DPAR_MAX_MINOR] == 5):
       return DISTRIBUTIONS.DIST_6MAJ5MIN20.value
     elif (params[DPAR_MAX_MINOR] == 2):
       return DISTRIBUTIONS.DIST_65MAJ20.value
     elif (params[DPAR_MAX_MINOR] == 1):
-      return DISTRIBUTIONS.DIST_66MAJ10.value
+      return DISTRIBUTIONS.DIST_66MAJ.value
+    elif (params[DPAR_MIN_MAJOR] == 4):
+      return DISTRIBUTIONS.DIST_64MAJ30.value
     else:
-      return DISTRIBUTIONS.DIST_6MAJ430.value
+      return DISTRIBUTIONS.DIST_6MAJ4MIN30.value
 
 
   def number6minor(self, params):
@@ -294,21 +328,25 @@ class Distribution:
       return DISTRIBUTIONS.DIST_6MIN322.value
     elif (params[DPAR_MIN_LEN] == 1):
       if (params[DPAR_MAX_MAJOR] == 5):
-        return DISTRIBUTIONS.DIST_51MAJ61.value
+        return DISTRIBUTIONS.DIST_5MAJ6MIN11.value
       elif (params[DPAR_MAX_MAJOR] == 1):
         return DISTRIBUTIONS.DIST_65MIN11.value
       elif (params[DPAR_MAX_MAJOR] == 3):
         return DISTRIBUTIONS.DIST_6MIN331.value
+      elif (params[DPAR_MIN_MINOR] == 4):
+        return DISTRIBUTIONS.DIST_64MIN21.value
       else:
-        return DISTRIBUTIONS.DIST_6MIN421.value
+        return DISTRIBUTIONS.DIST_4MAJ6MIN21.value
     elif (params[DPAR_MAX_MAJOR] == 5):
       return DISTRIBUTIONS.DIST_5MAJ6MIN20.value
     elif (params[DPAR_MAX_MAJOR] == 2):
       return DISTRIBUTIONS.DIST_65MIN20.value
     elif (params[DPAR_MAX_MAJOR] == 1):
-      return DISTRIBUTIONS.DIST_66MIN10.value
+      return DISTRIBUTIONS.DIST_66MIN.value
+    elif (params[DPAR_MIN_MINOR] == 4):
+      return DISTRIBUTIONS.DIST_64MIN30.value
     else:
-      return DISTRIBUTIONS.DIST_6MIN430.value
+      return DISTRIBUTIONS.DIST_4MAJ6MIN30.value
 
 
   def number7major(self, params):
@@ -316,7 +354,7 @@ class Distribution:
       return DISTRIBUTIONS.DIST_7MAJ222.value
     elif (params[DPAR_MIN_LEN] == 1):
       if (params[DPAR_MAX_MINOR] == 4):
-        return DISTRIBUTIONS.DIST_71MAJ41.value
+        return DISTRIBUTIONS.DIST_7MAJ4MIN11.value
       elif (params[DPAR_MAX_MINOR] == 1):
         return DISTRIBUTIONS.DIST_74MAJ11.value
       else:
@@ -324,7 +362,7 @@ class Distribution:
     elif (params[DPAR_MAX_MINOR] == 6):
       return DISTRIBUTIONS.DIST_7MAJ6MIN.value
     elif (params[DPAR_MAX_MINOR] == 5):
-      return DISTRIBUTIONS.DIST_7MAJ5MIN10.value
+      return DISTRIBUTIONS.DIST_7MAJ5MIN.value
     elif (params[DPAR_MAX_MINOR] == 4):
       return DISTRIBUTIONS.DIST_7MAJ4MIN20.value
     elif (params[DPAR_MAX_MINOR] == 3):
@@ -332,9 +370,9 @@ class Distribution:
     elif (params[DPAR_MAX_MINOR] == 2):
       return DISTRIBUTIONS.DIST_74MAJ20.value
     elif (params[DPAR_MAX_MINOR] == 1):
-      return DISTRIBUTIONS.DIST_75MAJ10.value
+      return DISTRIBUTIONS.DIST_75MAJ.value
     else:
-      return DISTRIBUTIONS.DIST_76MAJ00.value
+      return DISTRIBUTIONS.DIST_76MAJ.value
 
 
   def number7minor(self, params):
@@ -342,7 +380,7 @@ class Distribution:
       return DISTRIBUTIONS.DIST_7MIN222.value
     elif (params[DPAR_MIN_LEN] == 1):
       if (params[DPAR_MAX_MAJOR] == 4):
-        return DISTRIBUTIONS.DIST_71MIN41.value
+        return DISTRIBUTIONS.DIST_4MAJ7MIN11.value
       elif (params[DPAR_MAX_MAJOR] == 1):
         return DISTRIBUTIONS.DIST_74MIN11.value
       else:
@@ -350,7 +388,7 @@ class Distribution:
     if (params[DPAR_MAX_MAJOR] == 6):
       return DISTRIBUTIONS.DIST_6MAJ7MIN.value
     elif (params[DPAR_MAX_MAJOR] == 5):
-      return DISTRIBUTIONS.DIST_5MAJ7MIN10.value
+      return DISTRIBUTIONS.DIST_5MAJ7MIN.value
     elif (params[DPAR_MAX_MAJOR] == 4):
       return DISTRIBUTIONS.DIST_4MAJ7MIN20.value
     elif (params[DPAR_MAX_MAJOR] == 3):
@@ -358,9 +396,9 @@ class Distribution:
     elif (params[DPAR_MAX_MAJOR] == 2):
       return DISTRIBUTIONS.DIST_74MIN20.value
     elif (params[DPAR_MAX_MAJOR] == 1):
-      return DISTRIBUTIONS.DIST_75MIN10.value
+      return DISTRIBUTIONS.DIST_75MIN.value
     else:
-      return DISTRIBUTIONS.DIST_76MIN00.value
+      return DISTRIBUTIONS.DIST_76MIN.value
 
 
   def number8major(self, params):
@@ -372,11 +410,11 @@ class Distribution:
     elif (params[DPAR_MAX_MINOR] == 5):
       return DISTRIBUTIONS.DIST_8MAJ5MIN.value
     elif (params[DPAR_MAX_MINOR] == 4):
-      return DISTRIBUTIONS.DIST_8MAJ4MIN10.value
+      return DISTRIBUTIONS.DIST_8MAJ4MIN.value
     elif (params[DPAR_MAX_MINOR] == 1):
-      return DISTRIBUTIONS.DIST_84MAJ10.value
+      return DISTRIBUTIONS.DIST_84MAJ.value
     elif (params[DPAR_MAX_MINOR] == 0):
-      return DISTRIBUTIONS.DIST_85MAJ00.value
+      return DISTRIBUTIONS.DIST_85MAJ.value
     else:
       return DISTRIBUTIONS.DIST_8MAJ320.value
 
@@ -390,11 +428,11 @@ class Distribution:
     elif (params[DPAR_MAX_MAJOR] == 5):
       return DISTRIBUTIONS.DIST_5MAJ8MIN.value
     elif (params[DPAR_MAX_MAJOR] == 4):
-      return DISTRIBUTIONS.DIST_4MAJ8MIN10.value
+      return DISTRIBUTIONS.DIST_4MAJ8MIN.value
     elif (params[DPAR_MAX_MAJOR] == 1):
-      return DISTRIBUTIONS.DIST_84MIN10.value
+      return DISTRIBUTIONS.DIST_84MIN.value
     elif (params[DPAR_MAX_MAJOR] == 0):
-      return DISTRIBUTIONS.DIST_85MIN00.value
+      return DISTRIBUTIONS.DIST_85MIN.value
     else:
       return DISTRIBUTIONS.DIST_8MIN320.value
 
@@ -407,7 +445,7 @@ class Distribution:
     elif (params[DPAR_MAX_MINOR] == 2):
       return DISTRIBUTIONS.DIST_9MAJ220.value
     elif (params[DPAR_MAX_MINOR] == 0):
-      return DISTRIBUTIONS.DIST_94MAJ00.value
+      return DISTRIBUTIONS.DIST_94MAJ.value
     else:
       return DISTRIBUTIONS.DIST_9MAJ310.value
 
@@ -420,7 +458,7 @@ class Distribution:
     elif (params[DPAR_MAX_MAJOR] == 2):
       return DISTRIBUTIONS.DIST_9MIN220.value
     elif (params[DPAR_MAX_MAJOR] == 0):
-      return DISTRIBUTIONS.DIST_94MIN00.value
+      return DISTRIBUTIONS.DIST_94MIN.value
     else:
       return DISTRIBUTIONS.DIST_9MIN310.value
 
@@ -488,14 +526,14 @@ class Distribution:
 
     elif (params[DPAR_MAX_LEN] == 12):
       if (params[DPAR_MAX_MAJOR] == 12):
-        return DISTRIBUTIONS.DIST_12MAJ100.value
+        return DISTRIBUTIONS.DIST_12MAJ.value
       else:
-        return DISTRIBUTIONS.DIST_12MIN100.value
+        return DISTRIBUTIONS.DIST_12MIN.value
     elif (params[DPAR_MAX_LEN] == 13):
       if (params[DPAR_MAX_MAJOR] == 13):
-        return DISTRIBUTIONS.DIST_13MAJ000.value
+        return DISTRIBUTIONS.DIST_13MAJ.value
       else:
-        return DISTRIBUTIONS.DIST_13MIN000.value
+        return DISTRIBUTIONS.DIST_13MIN.value
 
     else:
       assert False
--- scripts/cluster/passes/PassTables.py
+++ scripts/cluster/passes/PassTables.py
@@ -16,6 +16,51 @@ class PassTables:
   # 16 * the distribution index + 4 * the relative player index +
   # the relative vulnerability index.
 
+  def read_any_pos_vul(self, txt_file, parts, distribution):
+    '''Read an Any.txt file directly in the distribution directory.'''
+    dist = distribution.name_to_number(parts[2])
+    assert parts[3] == "Any"
+    assert parts[4] == "txt"
+
+    base_index = 16 * dist
+    self.tables[base_index].read_file(txt_file)
+
+    print("ANY pos-vul parts", parts)
+
+    # Make up the other positions and vulnerabilities.
+    for index in range(base_index+1, base_index+16):
+      self.tables[index]= self.tables[base_index]
+
+  
+  def read_any_vul(self, txt_file, parts, distribution):
+    '''Read an Any.txt file in a distribution/position directory.'''
+    dist = distribution.name_to_number(parts[2])
+    pos = POSITIONS[parts[3]]
+    assert parts[4] == "Any"
+    assert parts[5] == "txt"
+
+    base_index = 16 * dist + 4 * pos
+    self.tables[base_index].read_file(txt_file)
+
+    print("ANY vul parts", parts)
+
+    # Make up the other vulnerabilities.
+    for index in range(base_index+1, base_index+4):
+      self.tables[index]= self.tables[base_index]
+
+  
+  def read_one(self, txt_file, parts, distribution, vulnerability):
+    '''Read one file in a distribution/position dir, one vul.'''
+    dist = distribution.name_to_number(parts[2])
+    assert parts[3] in POSITIONS
+    pos = POSITIONS[parts[3]]
+    vul = vulnerability.tag_to_relative(parts[4])
+    assert parts[5] == "txt"
+
+    index = 16 * dist + 4 * pos + vul
+    self.tables[index].read_file(txt_file)
+
+
   def __init__(self):
     self.tables = [Table() for _ in range(16 * len(DISTRIBUTION_NAMES))]
 
@@ -24,18 +69,17 @@ class PassTables:
 
     for txt_file in self.find_txt_files():
       parts = re.split('[/.]', txt_file)
-      if len(parts) != 6:
+
+      if len(parts) == 5:
+        self.read_any_pos_vul(txt_file, parts, distribution)
+      elif len(parts) == 6:
+        if parts[4] == "Any":
+          self.read_any_vul(txt_file, parts, distribution)
+        else:
+          self.read_one(txt_file, parts, distribution, vulnerability)
+      else:
         print("parts", parts)
-      assert len(parts) == 6
-      assert parts[3] in POSITIONS
-      assert parts[5] == "txt"
-
-      dist = distribution.name_to_number(parts[2])
-      pos = POSITIONS[parts[3]]
-      vul = vulnerability.tag_to_relative(parts[4])
-      index = 16 * dist + 4 * pos + vul
-
-      self.tables[index].read_file(txt_file)
+        assert False
 
     for i in (range(len(self.tables))):
       if self.tables[i].is_default():
