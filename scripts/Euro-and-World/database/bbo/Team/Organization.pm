039801d3c591b2267691154bb677d6ec9b991422
Soren Hein <soren.hein@gmail.com>
2024-06-02 12:59:02 +0200

    Added some organizations

--- scripts/Euro-and-World/database/bbo/Team/Organization.pm
+++ scripts/Euro-and-World/database/bbo/Team/Organization.pm
@@ -22,24 +22,67 @@ my @MULTI_WORDS =
 
 my %MULTI_TYPOS =
 (
+  ACBL => ['american contract bridge league'],
+  AfBF => ['african bridge federation'],
+  APBF => ['asia pacific bridge federation'],
+  AuBF => ['australian bridge federation'],
+  AZS => ['university sports association of poland'],
+  BFAME => ['bridge federation of asia & the middle east'],
+  BFI => ['bridge federation of india'],
+  BuBF => ['bulgarian bridge federation'],
+  CBAI => ['contract bridge association of ireland'],
+  CBF => ['canadian bridge federation'],
+  CBLT => ['contract bridge league of thailand'],
+  CCBA => ['chinese contract bridge association'],
+  CrBF => ['croatian bridge federation'],
+  CSB => ['south american bridge federation'],
+  CTCBA => ['chinese taipei contract bridge association'],
+  DBV => ['deutscher bridge-verband'],
+  EBL => ['european bridge league'],
   EBU => ['eng bridge union'],
+  FIGB => ['italian bridge federation'],
+  FFB => ['fédération francaise de bridge'],
+  FMB => ['fédération monegasque de bridge'],
+  FSB => ['fédération suisse de bridge'],
   GABSI => ['gabungan bridge seluruh indonesia'],
+  HKCBA => ['hong kong contract bridge association'],
+  IBF => ['israel bridge federation'],
   IBWI => ['ikatan bridge wanita indonesia'],
-  JBA => ['j.b.a.'],
+  JBA => ['j.b.a.', 'jordan bridge federation'],
+  MCBA => ['macau contract bridge association'],
+  NBB => ['dutch bridge federation'],
+  NBF => ['norsk bridgeforbund'],
+  NIBU => ['northern ireland bridge union'],
+  ÖBV => ['austrian bridge federation'],
+  PZBS => ['polish bridge union'],
+  PTBA => ['philippine tournament bridge association'],
+  SBF => ['svenska bridgeförbundet'],
+  SBU => ['scottish bridge union'],
+  TBF => ['turkish bridge federation'],
+  USBF => ['united states bridge federation'],
+  WBF => ['world bridge federation'],
+  WBU => ['welsh bridge union']
 );
 
 my @SINGLE_WORDS = qw(
-  APBF AZS
-  CBAI CBLT
-  EBU
-  FMB
+  ACBL AfBF APBF AuBF AZS
+  BFAME BFI BuBF
+  CBAI CBF CBLT CCBA CrBF CSB CTCBA
+  DBV
+  EBL EBU
+  FIGB FFB FMB FSB
   GABSI
-  IBWI
+  HKCBA
+  IBF IBWI
   JBA
   MCBA
-  NIBU
-  PTBA
-  SBTW SBU
+  NBB NBF NIBU
+  ÖBV
+  PZBS PTBA
+  SBF SBU
+  TBF
+  USBF
+  WBF
   WBU
 );
 
@@ -48,6 +91,7 @@ my %SINGLE_TYPOS =
   APBF => ['PABF'],
   CBAI => ['cabi'],
   FMB => ['fm'],
+  GABSI => ['iba'],
   'Jilin Sheng Qiaopa' => ['jilinshengqiaopa'],
   'Shanghai Contract Bridge Association' => ['scba']
 );
