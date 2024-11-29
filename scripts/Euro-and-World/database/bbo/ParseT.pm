43530ec570ae3dc98ead20f287934b383e7c6a70
Soren Hein <soren.hein@gmail.com>
2024-11-29 13:41:56 +0100

    Added new detection code; not hooked up

--- scripts/Euro-and-World/database/bbo/ParseT.pm
+++ scripts/Euro-and-World/database/bbo/ParseT.pm
@@ -17,8 +17,6 @@ use DateCalc;
 use LinksT;
 use EntryT;
 
-my $DEBUG_LINKS = 1;
-
 my @MEET_FIELDS = qw(ORGANIZATION SPONSOR COUNTRY REGION CITY LOCALITY 
   ORDINAL ORIGIN ZONE FORM SCORING GENDER AGE);
 
@@ -154,7 +152,7 @@ sub init_links
     }
   }
 
-  $self->check_consistency() if $DEBUG_LINKS;
+  $self->set_lookup_links();
 }
 
 
@@ -309,6 +307,34 @@ sub check_chapter
 }
 
 
+sub get_header_fields
+{
+  my ($self, $tname, $edition, $cumul) = @_;
+
+  my $errstr = "$tname, $edition:";
+
+  my $t_header = $self->{TOURNAMENT}{$tname};
+  my $t_edition = $t_header->{EDITIONS}{$edition};
+
+  $self->cumulate_fields($t_header, 
+    {}, { EDITIONS => 1 }, $errstr, $cumul);
+  $self->cumulate_fields($t_edition, 
+    {}, { MEET_TAG => 1, CHAPTERS => 1 }, $errstr, $cumul);
+
+  return unless exists $t_edition->{MEET};
+
+  my $meet = $t_edition->{MEET};
+  my $meet_tag = $t_edition->{MEET_TAG} // $edition;
+  my $m_header = $self->{MEET}{$meet};
+  my $m_edition = $m_header->{EDITIONS}{$meet_tag};
+
+  $self->cumulate_fields($m_header, 
+    {}, { EDITIONS => 1 }, $errstr, $cumul);
+  $self->cumulate_fields($m_edition, 
+    { YEAR => 1 }, {}, $errstr, $cumul);
+}
+
+
 sub get_all_fields
 {
   my ($self, $tname, $edition, $chapter, $cumul, $ch_data) = @_;
@@ -342,7 +368,7 @@ sub get_all_fields
 }
 
 
-sub check_consistency
+sub set_lookup_links
 {
   my ($self) = @_;
 
@@ -375,6 +401,9 @@ sub check_consistency
         if (exists $self->{MEET}{$meet}{EDITIONS}{$mtag})
         {
           $mchapter = $self->{MEET}{$meet}{EDITIONS}{$mtag};
+
+          push @{$self->{M_LINKS}{$meet}{$mtag}},
+            [ TNAME => $tournament, EDITION => $tag ];
         }
         else
         {
@@ -382,6 +411,9 @@ sub check_consistency
           $mtag = '';
         }
       }
+
+      $self->get_header_fields($tournament, $tag,
+        \%{$self->{T_HEADERS}{$tournament}{$tag}});
 
       for my $ctag (sort keys %{$edition->{CHAPTERS}})
       {
@@ -397,9 +429,8 @@ sub check_consistency
             $chapter->{DATE_START}, $chapter->{DATE_END});
         }
 
-        my (%cumul, %ch_data);
-        $self->get_all_fields($tournament, $tag, $ctag, 
-          \%cumul, \%ch_data);
+        check_chapter($edition->{CHAPTERS}{$chapter}, 
+          "$tournament, $tag, $ctag");
       }
     }
   }
@@ -616,6 +647,172 @@ sub get_edition_and_chapter
   else
   {
     warn $entry->bbono() . " not found (dist $lowest_dist): " .
+      "$tname, " . $entry->field('DATE_ADDED');
+    return ($tname, '', '');
+  }
+}
+
+
+sub get_tname_list
+{
+  my ($self, $meet, $tname, $entry, $tname_list) = @_;
+
+  # It's useful to screen by the year of the entry.
+  my $entry_year = $entry->field('YEAR');
+  if ($entry_year eq '')
+  {
+    $entry->field('DATE_ADDED') =~ /^(\d\d\d\d)/;
+    $entry_year = $1;
+  }
+
+  if (exists $self->{TOURNAMENT}{$tname})
+  {
+    for my $edition (keys %{$self->{TOURNAMENT}{EDITIONS}})
+    {
+      if ($edition =~ /^(\d\d\d\d)[A-Z]{0,1}$/)
+      {
+        next unless $1 eq $entry_year;
+      }
+      push @$tname_list, [ TNAME => $tname, EDITION => $edition ];
+    }
+  }
+  elsif ($meet ne '' && exists $self->{MEET}{$meet})
+  {
+    my $m = $self->{M_LINKS}{$meet};
+    for my $edition (keys %$m)
+    {
+      if ($edition =~ /^(\d\d\d\d)[A-Z]{0,1}$/)
+      {
+        next unless $1 eq $entry_year;
+      }
+      push @$tname_list, @{$m->{$edition}};
+    }
+  }
+  else
+  {
+    warn $entry->bbono() . " meet $meet, tname $tname not found";
+  }
+}
+
+
+sub str_or_equal
+{
+  my ($primary, $match) = @_;
+  return ($primary eq $match ? '=' : $primary);
+}
+
+
+sub update_chapter_match
+{
+  my ($self, $header, $target, $t_hits, 
+    $tname, $edition_str, $chapter_str, 
+    $best, $entry, $debug) = @_;
+
+  my $edition = $self->{TOURNAMENTS}{$tname}{EDITION}{$edition_str};
+  my $chapter = $edition->{CHAPTERS}{$chapter_str};
+  return unless ref($chapter) eq 'HASH';
+
+  my ($c_hits, $c_conflicts) = compatibility($chapter, $entry);
+  next unless $c_conflicts == 0;
+
+  my $hits = $t_hits + $c_hits;
+
+  my $opens = 
+    (exists $header->{GENDER} && $header->{GENDER} eq 'Open' ? 1 : 0) +
+    (exists $header->{AGE} && $header->{AGE} eq 'Open' ? 1 : 0);
+
+  my $dist = $target->distance(
+    $chapter->{DATE_START},
+    $chapter->{DATE_END});
+
+  if ($debug)
+  {
+    print "$tname, $edition_str, $chapter_str: " .
+      "Hits $hits, Open $opens, Dist $dist\n";
+  }
+
+  if (($dist < $best->{LOWEST_DIST}) ||
+      ($dist == $best->{LOWEST_DIST} && $hits > $best->{LOWEST_HITS}) ||
+      ($dist == $best->{LOWEST_DIST} && $hits == $best->{LOWEST_HITS} &&
+       $opens > $best->{LOWEST_OPENS}))
+  {
+    print "        SWITCHING: dist now $dist\n" if $debug;
+    $best->{LOWEST_DIST} = $dist;
+    $best->{LOWEST_TNAME} = $tname;
+    $best->{LOWEST_EDITION} = $edition_str;
+    $best->{LOWEST_CHAPTER} = $chapter_str;
+    $best->{LOWEST_HITS} = $hits;
+    $best->{LOWEST_OPENS} = $opens;
+    $best->{EQUAL_COLLISION} = 0;
+  }
+  elsif ($dist == 0 && 
+      $hits == $best->{LOWEST_HITS} && 
+      $opens == $best->{LOWEST_OPENS})
+  {
+    $best->{EQUAL_COLLISION} = 1;
+    $best->{COLLISION_STR} = $entry->bbono() . 
+      " double zero, ($tname, $edition_str, $chapter_str) vs (" .
+      str_or_equal($best->{LOWEST_TNAME}, $tname) . ", " .
+      str_or_equal($best->{LOWEST_EDITION}, $edition_str) . ", " .
+      str_or_equal($best->{LOWEST_CHAPTER}, $chapter_str) . ")";
+  }
+}
+
+
+sub get_edition_and_chapter_new
+{
+  my ($self, $meet, $tname, $entry, $debug) = @_;
+
+  my @tname_list;
+  $self->get_tname_list($meet, $tname, $entry, \@tname_list);
+  return ($tname, '', '') unless $#tname_list >= 0;
+
+  my $target = DateCalc->new();
+  $target->set_by_field($entry->field('DATE_ADDED'));
+
+  my %best;
+  $best{LOWEST_DIST} = 9999;
+  $best{LOWEST_HITS} = 0;
+  $best{EQUAL_COLLISION} = 0;
+  $best{COLLISION_STR} = '';
+
+  for my $tname_entry (@tname_list)
+  {
+    my $tname_cand = $tname_entry->{TNAME};
+    my $edition_cand = $tname_entry->{EDITION};
+
+    my $t = $self->{T_HEADERS}{$tname_cand}{$edition_cand};
+    my ($t_hits, $t_conflicts) = compatibility($t, $entry);
+    next if $t_conflicts > 0;
+
+    $t0 = time();
+    my $edition = 
+      $self->{TOURNAMENTS}{$tname_cand}{EDITION}{$edition_cand};
+    for my $chapter_str (keys %{$edition->{CHAPTERS}})
+    {
+      $self->update_header_match($t, $target, $t_hits,
+        $tname_cand, $edition_cand, $chapter_str,
+        \%best, $entry, $debug);
+    }
+    $times[1] += time() - $t0;
+  }
+
+  if ($best{EQUAL_COLLISION})
+  {
+    warn $best{COLLISION_STR};
+  }
+
+  if ($best{LOWEST_DIST} == 0)
+  {
+    # 'Within one week' would be 7.
+    return ($best{LOWEST_TNAME}, 
+        $best{LOWEST_EDITION}, 
+        $best{LOWEST_CHAPTER});
+  }
+  else
+  {
+    warn $entry->bbono() . 
+      " not found (dist $best{LOWEST_DIST}): " .
       "$tname, " . $entry->field('DATE_ADDED');
     return ($tname, '', '');
   }
