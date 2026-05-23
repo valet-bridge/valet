94c5a64cb39a493fdd1bd339862ee471da622831
Soren Hein <soren.hein@gmail.com>
2026-05-23 19:01:11 +0200

    Moved a bunch of stuff to Proton Drive

--- scripts/Euro-and-World/database/pnames/bbo/nlast.pl
+++
@@ -1,39 +0,0 @@
-#!perl
-use strict;
-use warnings;
-
-my $file = 'bothshort';
-my $lno = 0;
-open(my $fh, "<", $file) or die "Cannot open $file: $!";
-while (my $line = <$fh>)
-{
-  chomp $line;
-  $line =~ s///g;
-  my $line1 = $line;
-
-  $line = <$fh>;
-  chomp $line;
-  $line =~ s///g;
-  my $line2 = $line;
-
-  $line = <$fh>;
-  chomp $line;
-  $line =~ s///g;
-  my $line3 = $line;
-
-  $line = <$fh>;
-  chomp $line;
-  $line =~ s///g;
-  my $line4 = $line;
-
-  $lno += 4;
-
-  if ($line4 !~ /^\s*$/)
-  {
-    die "$lno: $line4";
-  }
-
-  print "$line3\n";
-}
-close $fh;
-
